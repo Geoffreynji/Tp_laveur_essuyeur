5d664c79c42b39556018eb73d0042881cfbb0870
Geoffrey Naji <geoffrey.naji@etud.univ-jfc.fr>
2024-10-02 09:13:51 +0200

    Last commit

--- src/main/java/lavage/PileDAssiettes.java
+++ src/main/java/lavage/PileDAssiettes.java
@@ -16,9 +16,16 @@ class PileDAssiettes {
 		return (myList.size() >= MAX);
 	}
 
-	public void push(Assiette assiette) throws InterruptedException {
+	synchronized public void push(Assiette assiette) throws InterruptedException {
+		//Il faut synchroniser cette méthode pour empêcher les threads d'y accéder en même temps. 
+		while(isFull())
+		{
+			wait();
+		}
 		myList.add(assiette);
 		System.out.printf("la pile contient %d assiettes%n", myList.size());
+		notifyAll();
+		
 	}
 
 	synchronized public Assiette pop() throws InterruptedException {
